2d57bd6153c8b7b005cdce1beabf53331acd82b2
jianming <2437341214@qq.com>
2020-05-16 22:54:12 +0800

    更新设计模式主类

---
+++ design-pattern/src/main/java/com/jianming/designpattern/DesignPatternApplication.java
@@ -0,0 +1,16 @@
+package com.jianming.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 该项目模块包含常见的设计模式及软件设计原则
+ */
+@SpringBootApplication
+public class DesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplication.class, args);
+    }
+
+}
